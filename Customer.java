5229228db01054debd664b37b4d6b67145db434c
NatanVentura <natanvent@gmail.com>
2022-11-26 20:52:43 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -16,18 +16,10 @@ public class Customer {
    public String getName (){
       return _name;
    }
-  
-  public String statement() {
-     double totalAmount = 0;
-     int frequentRenterPoints = 0;
-     Enumeration rentals = _rentals.elements();
-     String result = "Rental Record for " + getName() + "\n";
-     while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
-        Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
+  public int amountFor(Rental each){
+      int thisAmount = 0;
+      switch (each.getMovie().getPriceCode()) {
            case Movie.REGULAR:
               thisAmount += 2;
               if (each.getDaysRented() > 2)
@@ -42,6 +34,20 @@ public class Customer {
                  thisAmount += (each.getDaysRented() - 3) * 1.5;
                break;
         }
+    return thisAmount;
+  }
+  
+  public String statement() {
+     double totalAmount = 0;
+     int frequentRenterPoints = 0;
+     Enumeration rentals = _rentals.elements();
+     String result = "Rental Record for " + getName() + "\n";
+     while (rentals.hasMoreElements()) {
+        double thisAmount = 0;
+        Rental each = (Rental) rentals.nextElement();
+
+        //determine amounts for each line
+        thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
